1567fa4843f231e089d32d61475b6c564000c50b
jerseycoder <michelle@codeforamerica.org>
2017-07-28 02:50:47 +0000

    Basic stat display

--- rouletteTk.py
+++ rouletteTk.py
@@ -10,23 +10,32 @@ def exitProgram():
 
 
 def enter_num(event):
+	myFont = tkFont.Font(family="Helvetica",size=80,weight="bold")
 	camera.capture("wheel2.jpg")
 	img2 = ImageTk.PhotoImage(Image.open("wheel2.jpg"))
 	panel.configure(image=img2)
 	panel.image=img2
+	stats = win.create_text(650,100, text="stats",font=dayanaFont, fill="blue")
+	if num2display:	
+		#stats = win.create_text(650, 200, text=win.itemcget(num2display[0],"text"),font=myFont, fill="orange")
+		#win.delete(statnum)
+		win.itemconfig(statnum,text=str(len(num2display)))
+		#statnum=win.create_text(650,200, text=str(len(num2display)),font=dayanaFont, fill="blue")
 	numentered = v.get()
-	myFont = tkFont.Font(family="Helvetica",size=80,weight="bold")
-	# Make new text to show on screen and append to num2display list        
-	num2display.append(win.create_text(alignNum(v.get()), 50, text=v.get(), font=myFont, fill=numColor(numentered)))
-	# Display the new number and move the others down
-	for index, rollvalue in enumerate(num2display):
-		#check if the rollvalue is going to be half-off the screen
-		# If so, just roll it entirely off the screen
-		if win.coords(rollvalue)[1] > 1050:
-			win.move(rollvalue,0,200)
-		else:
-			win.move(rollvalue,0,100)
-		v.set("")
+	if v.get() != "":
+		# Make new text to show on screen and append to num2display list        
+		num2display.append(win.create_text(alignNum(v.get()), 50, text=v.get(), font=myFont, fill=numColor(numentered)))
+		# Display the new number and move the others down
+		for index, rollvalue in enumerate(num2display):
+			#check if the rollvalue is going to be half-off the screen
+			# If so, just roll it entirely off the screen
+			if win.coords(rollvalue)[1] > 1200:
+				win.move(rollvalue,0,200)
+			else:
+				win.move(rollvalue,0,100)
+			v.set("")
+			win.update
+	else:
 		win.update
 
 def numPadConvert(num2convert):
@@ -77,6 +86,8 @@ master.geometry('900x1440')
 master.title("Roulette Spin Tracker v1.0")
 tkrgb = "#%02x%02x%02x" % (255, 255, 230)
 win = Canvas(master, width=900, height=1440, bg=tkrgb)
+dayanaFont = tkFont.Font(family="Times",size=80,weight="bold")
+statnum = win.create_text(650,200, text="",font=dayanaFont, fill="blue")
 v = StringVar()
 camera=picamera.PiCamera()
 camera.resolution = (640, 480)
